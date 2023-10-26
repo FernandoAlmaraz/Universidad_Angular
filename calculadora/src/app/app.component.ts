bd893e67197f4f0c70738e036b780b006138c7f6
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-26 19:04:04 -0400

    calculadora terminada, usando lo aprendido

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  Titulo = 'Aplicacion de Calculadora';
+  resultado = 0;
+  A: number = 0;
+  B: number = 0;
+
+  valorA(event: Event) {
+    this.A = Number((<HTMLInputElement>event.target).value);
+  }
+  valorB(event: Event) {
+    this.B = Number((<HTMLInputElement>event.target).value);
+  }
+  sumar() {
+    return this.resultado = this.A + this.B;
+  }
 }

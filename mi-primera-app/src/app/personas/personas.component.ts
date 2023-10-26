f4a744c05e48a778745dd67ab40726e001d3318b
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-26 17:41:32 -0400

    event binding event push keyborad

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -19,8 +19,12 @@ export class PersonasComponent implements OnInit {
   ngOnInit() {
   }
   deshabilitar = false;
-  mensaje = "Persona no agregada"
+  mensaje = "Persona no agregada";
+  titulo = "";
   agregarPersona() {
     this.mensaje = "Persona agregada."
+  }
+  modificarTitulo(event: Event) {
+    this.titulo = (<HTMLInputElement>event.target).value;
   }
 }

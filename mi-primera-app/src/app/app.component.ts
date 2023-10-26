88f6a27a26613036b93bccbff83d36e62913e73c
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-26 16:42:29 -0400

    agregando etiquetas

--- mi-primera-app/src/app/app.component.ts
+++ mi-primera-app/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  title = 'Desde Angular';
   saludo = 'Hola a todos desde angular'
 }
--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styleUrls: ['./personas.component.css']
+  styleUrls: ['./personas.component.css'],
+  template: '<div style="text-align: center;"> < hr ><h1>Listado de personas</ h1 ><app-persona > </app-persona></div>'
 })
 export class PersonasComponent implements OnInit {
 

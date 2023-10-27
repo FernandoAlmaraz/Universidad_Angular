b5c6c7b76406abbaa4d9fec454f0f568ed16b9c8
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-27 16:10:20 -0400

    mandando inf del comp padre al hijo

--- listadoPersonas/src/app/app.module.ts
+++ listadoPersonas/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PersonaComponent } from './persona/persona.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
